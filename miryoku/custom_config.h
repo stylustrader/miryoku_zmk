1b6687980e14ac80f91bc9ac689547b6fc2ebeb2
stylustrader <130052407+stylustrader@users.noreply.github.com>
2024-08-02 16:30:20 +0900

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -5,4 +5,4 @@
 &none,             &none,             &none,             &none,             &none,             &none,             &kp KP_N7,         &kp KP_N8,         &kp KP_N9,         &none,             \
 &kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         &none,             &none,             &kp KP_N4,         &kp KP_N5,         &kp KP_N6,         &none,             \
 &none,             &none,             &none,             &none,             &none,             &none,             &kp KP_N1,         &kp KP_N2,         &kp KP_N3,         &none,             \
-&none,             &none,             &none,             &none,             &none,             &none,             &none,             &none,             &none,             &none
+&none,             &none,             &none,             &none,             &none,             U_BTN1,            U_BTN2,            U_BTN3,            &none,             &none
