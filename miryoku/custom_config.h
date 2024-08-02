76ff98ebda25a1938b3af66701398e54c5c31925
stylustrader <130052407+stylustrader@users.noreply.github.com>
2024-08-02 16:28:56 +0900

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -2,6 +2,7 @@
 // https://github.com/manna-harbour/miryoku
 
 #define MIRYOKU_LAYER_MOUSE \
-&none,             &none,             &none,             &none,             &none,             &none,            &kp KP_N7,             &kp KP_N8,            &kp KP_N9,            &none,             \
-&kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         &none,             &none,            &kp KP_N4,             &kp KP_N5,            &kp KP_N6,            &none,             \
-&none,             &none,             &none,             &none,             &none,             &none,            &kp KP_N1,             &kp KP_N2,            &kp KP_N3,            &none,             \
+&none,             &none,             &none,             &none,             &none,             &none,             &kp KP_N7,         &kp KP_N8,         &kp KP_N9,         &none,             \
+&kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         &none,             &none,             &kp KP_N4,         &kp KP_N5,         &kp KP_N6,         &none,             \
+&none,             &none,             &none,             &none,             &none,             &none,             &kp KP_N1,         &kp KP_N2,         &kp KP_N3,         &none,             \
+&none,             &none,             &none,             &none,             &none,             &none,             &none,             &none,             &none,             &none
