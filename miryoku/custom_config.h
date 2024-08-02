adb1f4aa61ad0cdb96b98d29fad436a667c60cdc
stylustrader <130052407+stylustrader@users.noreply.github.com>
2024-08-02 14:42:49 +0900

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -2,4 +2,13 @@
 // https://github.com/manna-harbour/miryoku
 
 #define MIRYOKU_CLIPBOARD=MACSWAP
+#define MIRYOKU_CLIPBOARD_MACSWAP
+MIRYOKU_CLIPBOARD=MAC
+MIRYOKU_CLIPBOARD_WIN
+
 #define MIRYOKU_KLUDGE_MOUSEKEYSPR
+
+#define MIRYOKU_LAYER_NAV \
+U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
+KC_LGUI,           KC_LALT,           KC_LCTL,           KC_LSFT,           U_NA,              U_NA,              KC_LEFT,           KC_DOWN,           KC_UP,             KC_RGHT,           \
+U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              KC_INS,            KC_HOME,           KC_PGDN,           KC_PGUP,           KC_END,            \
