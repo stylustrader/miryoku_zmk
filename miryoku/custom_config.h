f6dfb933a42061c189e0e2e3e377b74adf4ec898
stylustrader <130052407+stylustrader@users.noreply.github.com>
2024-08-02 20:20:56 +0900

    Update custom_config.hm

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -3,6 +3,7 @@
 
 #define MIRYOKU_LAYER_MOUSE \
 &none,             &none,             &none,             &none,             &none,             U_RDO,              U_PST,             U_CPY,             U_CUT,             U_UND,             \
-&kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         &none,             U_WH_U,             U_WH_L,            U_MS_U,            U_WH_R,            &none,             \
-&none,             &none,             &none,             &none,             &none,             U_WH_D,             U_MS_L,            U_MS_D,            U_MS_R,            &none,             \
+&kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         &none,             U_WH_D,             U_WH_R,            U_MS_U,            U_WH_L,            &none,             \
+&none,             &none,             &none,             &none,             &none,             U_WH_U,             U_MS_L,            U_MS_D,            U_MS_R,            &none,             \
 &none,             &none,             &none,             &none,             &none,             U_BTN1,             U_BTN2,            U_BTN3,            &none,             &none
+
