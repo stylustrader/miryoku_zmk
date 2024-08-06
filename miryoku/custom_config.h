62a937b4e14027ede592c214920915701acb1c39
stylustrader <130052407+stylustrader@users.noreply.github.com>
2024-08-06 13:15:55 +0900

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -7,3 +7,9 @@
 &none,             &none,             &none,             &none,             &none,             U_WH_U,             U_MS_L,            U_MS_D,            U_MS_R,            &none,             \
 &none,             &none,             &none,             &none,             &none,             U_BTN1,             U_BTN2,            U_BTN3,            &none,             &none
 
+#define MIRYOKU_ALTERNATIVES_NAV \
+&none,             &none,             &none,             &none,             &none,             U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
+&kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              &kp HOME,          &kp PG_UP,         &kp UP,            &kp PG_DN,         &kp INS,         \
+&none,             &none,             &none,             &none,             &none,             &kp END,           &kp LEFT,          &kp DOWN,          &kp RIGHT,         &u_caps_word,           \
+&none,             &none,             &none,             &none,             &none,             &kp RET,           &kp BSPC,          &kp DEL,           &none,             &none
+
